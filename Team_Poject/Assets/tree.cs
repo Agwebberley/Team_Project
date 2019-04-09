7933997e16d0e7f77ee86aa8abc04b9e96e7bd54
aubswallace <49454870+aubswallace@users.noreply.github.com>
2019-04-09 13:02:07 -0700

    start

---
+++ Team_Poject/Assets/tree.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class tree : MonoBehaviour {
+
+    public int health = 100;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "axe")
+        {
+            health = health - 10;
+            Debug.Log("It works");
+        }
+
+        if (health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
