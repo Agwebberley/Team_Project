cd84848b01f0cf2b9c12ae88d15142ad2e39152d
CenjaminBreech <49005743+CenjaminBreech@users.noreply.github.com>
2019-04-30 12:06:45 -0700

    Added 4 Scripts

---
+++ Team_Poject/Assets/Script/PlayerInventory.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+    public int wood, nail, stick, stone, rope; //basic items (re-used)
+    public int axeHead, shovel, key, saw; //unique items 
+
+}
